3289ec110bfa31ed056c8119c868e1988f088e39
maxiking203 <Maximilian.Sachmann@gmx.de>
2019-05-19 16:18:41 +0200

    added a test for changing from BookshelfView to BookView

--- hintreader/test/widget_test.dart
+++ hintreader/test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hintreader/main.dart';
+import 'package:hintreader/views/BookView.dart';
 
 void main() {
   testWidgets('Bookshelf display test', (WidgetTester tester) async {
@@ -16,4 +17,22 @@ void main() {
     expect(find.text("My bookshelf"), findsOneWidget);
     expect(find.byType(FlatButton), findsOneWidget);
   });
+
+
+  testWidgets('Change to BookvIew test', (WidgetTester tester) async {
+
+
+    await tester.pumpWidget(MyApp());
+    expect(find.text("My bookshelf"), findsOneWidget);
+    await tester.pumpAndSettle();
+    expect(find.byType(FlatButton), findsWidgets);
+    await tester.tap(find.byType(FlatButton));
+    await tester.pumpAndSettle();
+    expect(find.byType(ListView), findsOneWidget);
+    expect(find.byIcon(Icons.wb_sunny), findsOneWidget);
+    expect(find.text("A"), findsOneWidget);
+    expect(find.text("a"), findsOneWidget);
+  });
+  
+  
 }
